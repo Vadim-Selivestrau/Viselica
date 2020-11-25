76a1fadab32e2c82a5c818cbd8136cf7cf1ecad6
Vadim-Selivestrau <selivestrau2003@gmail.com>
2020-11-26 00:24:24 +0300

    add works

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
+import random
 
+from words import words
 pygame.init()  # initialisation in Windows and Linux, Mac
+# from game_object import GameObject
+# from text_object import TextObject
 
 sc_width = 800
 sc_hight = 420
 sc = pygame.display.set_mode((sc_width, sc_hight))
 WHITE = (255, 255, 255)
 GRAY = (80, 80, 80)
+# work with word
+def choice_word(words):
+    word = random.choice(words)
+    return word
 
-word = "notebook"  # test word
+
+word = choice_word(words)
 length_word = len(word)
+
 mistake_counter = 6
+
+# work with image
 icon = pygame.image.load('img\icon\pogonya.jpg')
 pygame.display.set_icon(icon)  # icon application
 death1 = pygame.image.load('img\Viselica\death1.png')
 death2 = pygame.image.load('img\Viselica\death2.png')
-death3 = pygame.image.load('img\Viselica\death3.png' )
+death3 = pygame.image.load('img\Viselica\death3.png')
 death4 = pygame.image.load('img\Viselica\death4.png')
 death5 = pygame.image.load('img\Viselica\death5.png')
 death6 = pygame.image.load('img\Viselica\death6.png')
@@ -34,6 +46,17 @@ death6.set_colorkey(WHITE)
 location_image = (-22, 25, 50, 50)
 pygame.display.set_caption("Viselica")  # заголовок окна
 
+
+# work with button
+# class Button():
+#     def __init__(self, x, y, w, h, txt,): #initialisation atributes x,y-coord w,h(px) and txt
+#         self.x = x
+#         self.y = y
+#     def active_button(self,):
+
+
+
+
 sc.fill(GRAY)
 pygame.draw.aaline(sc, WHITE,
                    [322, 70],
@@ -42,22 +65,21 @@ pygame.draw.aaline(sc, WHITE,
                    [0, 70],
                    [800, 70])
 
-x: int =20
-y: int =0
-for i in range(0, length_word):     #drow line from secret word
+x: int = 20
+y: int = 0
+for i in range(0, length_word):  # drow line from secret word
     pygame.draw.aaline(sc, WHITE,
                        [y, 40],
                        [x, 40])
-    y+=80
-    x+=80
-
+    y += 80
+    x += 80
 
 
 def runGame():
     Game: bool = True
     while Game:
         for i in pygame.event.get():
-            if i.type == pygame.QUIT: #or mistake_counter == 6  # END GAME
+            if i.type == pygame.QUIT:  # or mistake_counter == 6  # END GAME
                 pygame.quit()
                 quit()
 
@@ -79,12 +101,6 @@ def output_image(counter):
         sc.blit(death5, location_image)
     elif (counter == 6):
         sc.blit(death6, location_image)
-
-
-
-
-
-
 
 
 runGame()
