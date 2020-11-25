ef4516dbe599dd78253f7969f944b1463d4c940e
Vadim-Selivestrau <selivestrau2003@gmail.com>
2020-11-25 09:02:45 +0300

    add reziable(not work) images and another

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pygame
 
 pygame.init()   #initialisation in Windows and Linux, Mac
-sc = pygame.display.set_mode((800, 420))
+sc_width = 800
+sc_hight = 420
+sc = pygame.display.set_mode((sc_width, sc_hight),pygame.RESIZABLE)
 WHITE = (255, 255, 255)
 ORANGE = (255, 150, 100)
 GRAY = (80, 80, 80)
-word = ""
+word = "notebook"
+mistake = 0
+icon = pygame.image.load('img\icon\pogonya.jpg')
+pygame.display.set_icon(icon)   #icon application
 death1 = pygame.image.load('img\Viselica\death1.png')
 death2 = pygame.image.load('img\Viselica\death2.png')
 death3 = pygame.image.load('img\Viselica\death3.png')
 death4 = pygame.image.load('img\Viselica\death4.png')
 death5 = pygame.image.load('img\Viselica\death5.png')
 death6 = pygame.image.load('img\Viselica\death6.png')
-death1.set_colorkey((255, 255, 255))
+death1 = pygame.transform.scale(death6, (sc_width // 2, sc_hight * 6 // 7))
+death1.set_colorkey((255, 255, 255))    #transparent background
 death2.set_colorkey((255, 255, 255))
 death3.set_colorkey((255, 255, 255))
 death4.set_colorkey((255, 255, 255))
 death5.set_colorkey((255, 255, 255))
 death6.set_colorkey((255, 255, 255))
 death = 0
-lokate_image = (0, 70, 50, 50)
+lokate_image = (-22, 25, 50, 50)
 pygame.display.set_caption("viselica")  # заголовок окна
 runGame = True
 sc.fill(GRAY)
 pygame.draw.aaline(sc, WHITE,
-                   [220, 70],
-                   [220, 420])
+                   [322, 70],
+                   [322, 420])
 pygame.draw.aaline(sc, WHITE,
                    [0, 70],
                    [800, 70])
